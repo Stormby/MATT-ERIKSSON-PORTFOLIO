8aa1d0d15244b7bcf13263e8da01d93f769ebe17
Matt Eriksson <matteriksson@gmail.com>
2019-08-29 11:31:22 +0100

    Javascript file added

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//This is a single line comment, started with two slashes
+
+/* This is a multi-line comment
+Everything in here has no effect.
+var test = 'This will not actually create a variable';
+*/
+ //Everything after the slashes has no effect
+
+ console.log("Hello world")
